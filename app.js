4d509603efbbe17fa37655b42f4fe7eea9a07532
FacuMaseras <facundo.maseras@gmail.com>
2022-08-01 16:08:43 -0300

    Cambio en register

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/promociones', (req,res)=>{
     res.sendFile(__dirname + '/views/promociones.html');
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
