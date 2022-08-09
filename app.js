af2904a7b0872dffc1d91968e60f5dae4c5d5c8c
Axel Caceres Prayones <axelcp1997@gmail.com>
2022-08-09 17:32:54 -0400

    EJS Implementado v0.1

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.set('view engine' , 'ejs')
+app.set('views' , './views')
 app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 
@@ -8,36 +10,41 @@ app.listen(port,  () => {
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.render(__dirname + '/views/index.ejs');
 });
 
 app.get('/promociones', (req,res)=>{
-    res.sendFile(__dirname + '/views/promociones.html');
+    res.render(__dirname + '/views/products/promociones.ejs');
 });
 
 app.get('/vinos', (req,res)=>{
-    res.sendFile(__dirname + '/views/vinos.html');
+    res.render(__dirname + '/views/products/vinos.ejs');
 });
 
 app.get('/cervezas', (req,res)=>{
-    res.sendFile(__dirname + '/views/cervezas.html');
+    res.render(__dirname + '/views/products/cervezas.ejs');
 });
 
 app.get('/licores', (req,res)=>{
-    res.sendFile(__dirname + '/views/licores.html');
+    res.render(__dirname + '/views/products/licores.ejs');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.render(__dirname + '/views/users/login.ejs');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.render(__dirname + '/views/users/register.ejs');
 });
 
 app.get('/carrito', (req,res)=>{
-    res.sendFile(__dirname + '/views/carrito.html');
+    res.render(__dirname + '/views/products/carrito.ejs');
 });
+
+app.get('/edicionProductos', (req,res)=>{
+    res.render(__dirname + '/views/products/edicionProductos.ejs');
+});
+
 
 
 
