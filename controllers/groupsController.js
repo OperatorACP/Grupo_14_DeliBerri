cc2d53479ce7caecc9d04ccf370b9b8cc41af718
Axel Caceres Prayones <axelcp1997@gmail.com>
2022-08-22 13:30:23 -0400

    Modificaciones Miscelaneas

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -2,58 +2,58 @@ const fs = require('fs');
 const path = require('path');
 const jsonTable = require('../database/jsonTable');
 
-const groupsModel = jsonTable('groups');
+const productosModel = jsonTable('productos');
 
 module.exports = {
     index: (req, res) => {
 
-        let groups = groupsModel.all()
+        let productos = productosModel.all()
 
-        res.render('groups/index',  { groups });
+        res.render('productos/index',  { productos });
     },
     create: (req, res) => {
-        res.render('groups/create');
+        res.render('productos/create');
     },
     store: (req, res) => {
 
         let group = req.body;
 
-        groupId = groupsModel.create(group);
+        groupId = productosModel.create(group);
 
-        res.redirect('/groups/' + groupId);
+        res.redirect('/productos/' + groupId);
     },
     edit: (req, res) => {
-        let group = groupsModel.find(req.params.id)
+        let group = productosModel.find(req.params.id)
         let categories = categoriesModel.all();
 
-        res.render('groups/edit', { group, categories });
+        res.render('productos/edit', { group, categories });
     },
     update: (req, res) => {
         let group = req.body;
 
         group.id = req.params.id;
 
-        groupId = groupsModel.update(group);
+        groupId = productosModel.update(group);
 
-        res.redirect('/groups/' + groupId)
+        res.redirect('/productos/' + groupId)
     },
     show: (req, res) => {
-        let group = groupsModel.find(req.params.id);
+        let group = productosModel.find(req.params.id);
 
-        res.render('groups/detail', { group });
+        res.render('productos/detail', { group });
     },
     destroy: (req, res) => {
 
-        let group = groupsModel.find(req.params.id);
-        let imagePath = path.join(__dirname, '../public/img/groups/' + group.image);
+        let group = productosModel.find(req.params.id);
+        let imagePath = path.join(__dirname, '../public/img/' + group.image);
         
-        groupsModel.delete(req.params.id);
+        productosModel.delete(req.params.id);
 
         if (fs.existsSync(imagePath)) {
             fs.unlinkSync(imagePath)
         }
 
-        res.redirect('/groups')
+        res.redirect('/productos')
     },
     search: (req, res) => {
         
@@ -63,6 +63,6 @@ module.exports = {
 
         // Envío los grupos y lo que busco el usuario a la vista
 
-        res.render('groups/search', {});
+        res.render('productos/search', {});
     },
 }
