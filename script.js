1b525c5b291c974233599d856894c455d9b1bb26
Eduardo Virgilio <55003926+digitalhedu@users.noreply.github.com>
2022-09-02 17:20:52 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav-menu')
-
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('activate');
-    navMenu.classList.toggle('activate');
-})
