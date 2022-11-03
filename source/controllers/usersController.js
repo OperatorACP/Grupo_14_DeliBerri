4cfee1074fc6465c6ac25cc3cad4d65ad74c538c
Axel Caceres Prayones <axelcp1997@gmail.com>
2022-11-02 23:35:15 -0300

    CRUD Usuario primeras implementaciones

--- source/controllers/usersController.js
+++ source/controllers/usersController.js
@@ -1,92 +1,93 @@
+const db = require("../database/models")
 const fs = require("fs");
-const { userInfo } = require("os");
-const path = require("path");
-const User = require("../models/user");
+const user = require('../routes/usersRoutes');
 const { validationResult } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 
+
 const userController = {
-  register: (req, res) => {
-    return res.render("users/register");
-  },
-
-  processingRegister: (req, res) => {
-    const resultValidation = validationResult(req);
-    if (resultValidation.errors.length > 0) {
-      return res.render("users/register", {
-        errors: resultValidation.mapped(),
-        oldData: req.body,
-      });
-    }
-    let userInDB = User.findByField("email", req.body.email);
-    if (userInDB) {
-      return res.render("users/register", {
-        errors: {
-          email: {
-            msg: "Este email ya esta registrado",
+    register: (req, res) => {
+      return res.render("users/register", {user});
+    },
+  
+    processingRegister: (req, res) => {
+      const resultValidation = validationResult(req);
+      if (resultValidation.errors.length > 0) {
+        return res.render("users/register", {
+          errors: resultValidation.mapped(),
+          oldData: req.body,
+        });
+      }
+      let userInDB = db.user.findByField("email", req.body.email);
+      if (userInDB) {
+        return res.render("users/register", {
+          errors: {
+            email: {
+              msg: "Este email ya esta registrado",
+            },
           },
-        },
-        oldData: req.body,
-      });
-    }
-    let userToCreate = {
-      ...req.body,
-      password: bcryptjs.hashSync(req.body.password, 10),
-      avatar: req.file ? req.file.filename : null,
-    };
-    delete userToCreate.confirm_password;
-    let userCreated = User.create(userToCreate);
-    return res.redirect("/profile");
-  },
-
-  login: (req, res) => {
-    return res.render("users/login");
-  },
-
-  loginProcess: (req, res) => {
-    let userToLogin = User.findByField("email", req.body.email);
-
-    if (userToLogin) {
-      let isThePasswordOk = bcryptjs.compareSync(
-        req.body.password,
-        userToLogin.password
-      );
-      if (isThePasswordOk) {
-        delete userToLogin.password;
-        req.session.userLogged = userToLogin;
-        if (req.body.remember_user) {
-          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 2 });
+          oldData: req.body,
+        });
+      }
+      let userToCreate = {
+        ...req.body,
+        password: bcryptjs.hashSync(req.body.password, 10),
+        avatar: req.file ? req.file.filename : null,
+      };
+      delete userToCreate.confirm_password;
+      let userCreated = db.user.create(userToCreate);
+      return res.redirect("/profile");
+    },
+  
+    login: (req, res) => {
+      return res.render("users/login",{user});
+    },
+  
+    loginProcess: (req, res) => {
+      let userToLogin = db.user.findByField("email", req.body.email);
+  
+      if (userToLogin) {
+        let isThePasswordOk = bcryptjs.compareSync(
+          req.body.password,
+          userToLogin.password
+        );
+        if (isThePasswordOk) {
+          delete userToLogin.password;
+          req.session.userLogged = userToLogin;
+          if (req.body.remember_user) {
+            res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 2 });
+          }
+          return res.redirect("/profile");
         }
-        return res.redirect("/profile");
+        return res.render("users/login", {
+          errors: {
+            email: {
+              msg: "Las credenciales son invalidas.",
+            },
+          },
+        });
       }
       return res.render("users/login", {
         errors: {
           email: {
-            msg: "Las credenciales son invalidas.",
+            msg: "No se encuentra este email en nuestra base de datos.",
           },
         },
       });
+    },
+  
+    profile: (req, res) => {
+      return res.render("users/profile", {
+        user: req.session.userLogged,
+      });
+    },
+  
+    logout: (req, res) => {
+      res.clearCookie("userEmail");
+      req.session.destroy();
+      return res.redirect("/");
     }
-    return res.render("users/login", {
-      errors: {
-        email: {
-          msg: "No se encuentra este email en nuestra base de datos.",
-        },
-      },
-    });
-  },
-
-  profile: (req, res) => {
-    return res.render("users/profile", {
-      user: req.session.userLogged,
-    });
-  },
-
-  logout: (req, res) => {
-    res.clearCookie("userEmail");
-    req.session.destroy();
-    return res.redirect("/");
-  }
-};
-
-module.exports = userController;
+  };
+  
+  module.exports = userController;
+  
--- source/routes/usersRoutes.js
+++ source/routes/usersRoutes.js
@@ -2,19 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const usersController = require("../controllers/usersController");
-
 const uploadFile = require("../middlewares/multerMiddleware");
 const validations = require("../middlewares/validateRegisterMiddleware");
 const guestMiddleware = require("../middlewares/guestMiddleware");
 const authMiddleware = require("../middlewares/authMiddleware");
 
-router.get("/register", guestMiddleware, usersController.register); // href /usuarios/register
+router.get("/register", guestMiddleware, usersController.register); 
 router.post("/register",
   uploadFile.single("avatar"),
   usersController.processingRegister
 );
-router.get("/login", guestMiddleware, usersController.login); // href /usuarios/login
-router.post("/login", usersController.loginProcess); // href /usuarios/login
+router.get("/login", guestMiddleware, usersController.login); 
+router.post("/login", usersController.loginProcess); 
 router.get("/profile", authMiddleware, usersController.profile);
 router.get("/logout", usersController.logout);
 //router.post('/access', usersController.access ); // action /usuarios/access
