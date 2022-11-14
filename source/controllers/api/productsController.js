58c488662d7f555f6aa8533483ffc618ef21bc6e
Axel Caceres Prayones <axelcp1997@gmail.com>
2022-11-14 16:35:31 -0300

    Modificado Structure.sql para reflejar datos correctos

--- source/controllers/api/productsController.js
+++ source/controllers/api/productsController.js
@@ -20,10 +20,57 @@ const apiProductsController= {
     })
   },
   
+  detail: (req, res) => {
+    db.product.findByPk(req.params.id, {
+        /*include: [{association: "status"}, {association: "categories"}]*/
+    })
+    .then(product => {
+        return res.status(200).json({
+         url: product.image, 
+         status: 200,   
+         product: product,
+         
+        })
+    })
+   .catch(err => {
+    res.send(err)
+})
+},
+category: (req, res) => {
+    db.category.findAll()
+    .then(categories => {
+        return res.status(200).json({
+            count: categories.length,
+            categories: categories,
+        })
+    })
+},
+
+lastProduct: (req, res) => {
+
+    db.product.findAll({
+        // include: ["Category"],
+        order: [
+            ["id", "DESC"],
+        ],
+        limit: 1
+    })
+        .then(product => {
+            return res.status(200).json({
+                url: product.image, 
+                status: 200,   
+                product: product,
+                
+               })
+           
+        })
+        .catch(err => {
+            res.send(err)
+        })
+
+}
 
 
-
-  
   }
   
   
--- source/routes/api/productsRoutes.js
+++ source/routes/api/productsRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const apiProductsController = require('../../controllers/api/productsController');
 
 router.get('/', apiProductsController.index)
-// router.get('/categories', apiProductsController.category)
+ router.get('/categories', apiProductsController.category)
 // router.get('/status', apiProductsController.status)
 // router.get("/lastproduct", apiProductsController.lastProduct);
 // router.get('/:id', apiProductsController.detail)
